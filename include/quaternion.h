90e5c594bd467b960896e4b5e220ec3e5ee469c5
Haruaki Tanaka <ht.geek.within.sight+jp.co.bmcomp@gmail.com>
2024-02-06 17:25:25 +0900

    Add document example

--- include/quaternion.h
+++ include/quaternion.h
@@ -1,6 +1,10 @@
+/*!
+ \file
+ */
 #ifndef QUATERNION_H
 #define QUATERNION_H
 
+/*! Quaternion struct. */
 struct Quaternion {
   double w;
   double x;
@@ -8,13 +12,41 @@ struct Quaternion {
   double z;
 };
 
+/*!
+ * Add quaternions.
+ *
+ * \param lhs left-hand side quaternion.
+ * \param rhs right-hand side quaternion.
+ * \param[out] out \a lhs + \a rhs.
+ */
 void quaternion_add(const struct Quaternion *lhs, const struct Quaternion *rhs,
                     struct Quaternion *out);
+/*!
+ * Subtract quaternions.
+ *
+ * \param lhs left-hand side quaternion.
+ * \param rhs right-hand side quaternion.
+ * \param[out] out \a lhs - \a rhs.
+ */
 void quaternion_sub(const struct Quaternion *lhs, const struct Quaternion *rhs,
                     struct Quaternion *out);
-void quaternion_conjugate(const struct Quaternion *lhs, struct Quaternion *out);
+/*!
+ * Take conjugate of a quaternion.
+ *
+ * \param in input quaternion.
+ * \param[out] out conjugate of \a in.
+ */
+void quaternion_conjugate(const struct Quaternion *in, struct Quaternion *out);
+
+/*!
+ * Multiply quaternions.
+ *
+ * \param lhs left-hand side quaternion.
+ * \param rhs right-hand side quaternion.
+ * \param[out] out \a lhs * \a rhs.
+ */
 void quaternion_mul(const struct Quaternion *restrict lhs,
                     const struct Quaternion *restrict rhs,
                     struct Quaternion *restrict out);
 
-#endif
+#endif  // QUATERNION_H
